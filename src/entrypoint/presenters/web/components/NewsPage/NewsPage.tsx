310a8802c299347351457853372dc07bfd19ad55
agustin <adammiano@itba.edu.ar>
2021-02-19 17:35:03 -0300

    add component logic NewsPage

--- src/entrypoint/presenters/web/components/NewsPage/NewsPage.tsx
+++ src/entrypoint/presenters/web/components/NewsPage/NewsPage.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import useStyles from "./styles";
+import News from "@domain/News/News";
+import useStyles from "./styles";
 // import { useTranslation } from 'react-i18next';
+import { container } from "@container-inversify";
+import { TYPES } from "@constants/types";
+import NewsService from "@configuration/usecases/NewsService";
+import { CircularProgress } from "@material-ui/core";
+
+const newsService = container.get<NewsService>(TYPES.NewsService);
 
 export default function NewsPage() {
-    const { id } = useParams<{id:string}>();
+    const { id } = useParams<{ id: string }>();
+    const idNumber = Number(id)
 
+    const classes = useStyles();
+
+    const [news, setNews] = useState<News>(undefined);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (idNumber !== NaN) {
+            newsService.getNewsGetListUseCase().get(idNumber)
+                .then((newsResponce) => setNews(newsResponce))
+                .catch((error) => console.log(error))//TODO maybe do somethin else
+                .finally(() => setLoading(false))
+        }
+    }, [idNumber])
+
+    if (idNumber === NaN || (loading === false && news === undefined)) {
+        return <>404 para el id:{id}</>//TODO render 404
+    }
+    if (loading === true) {
+        return (
+            <div className={classes.containerSpinner}>
+                <CircularProgress className={classes.spinner} size="10" />
+            </div>
+        );
+    }
     return (
         <>{id} es el id</>
     )
---
+++ src/entrypoint/presenters/web/components/NewsPage/styles.jsx
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles(theme => ({
+    containerSpinner: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'center',
+        height: 'auto',
+        alignItems: 'center',
+        marginTop: '5%',
+        minHeight: '500px',
+    },
+    spinner: {
+        width: '20%'
+    }
+}));
