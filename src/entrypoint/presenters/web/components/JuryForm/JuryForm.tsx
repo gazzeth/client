a2fea52135761ee41b43c7fec47b1dddc3d4c80d
Agustin Dammiano <adammiano@itba.edu.ar>
2021-05-25 10:58:20 -0300

    improve jury form

--- src/entrypoint/presenters/web/components/JuryForm/JuryForm.tsx
+++ src/entrypoint/presenters/web/components/JuryForm/JuryForm.tsx
@@ -4,20 +4,106 @@ import useStyles from "./styles";
 import { useTranslation } from 'react-i18next';
 import SelectTopic from "../SelectTopic/SelectTopic";
 import Topic from "@domain/models/Topic/Topic";
+import NumberFormat from 'react-number-format';
+import { Button, IconButton, TextField, Typography } from "@material-ui/core";
+import CloseIcon from '@material-ui/icons/Close';
+import classnames from "classnames";
+import LockInfo from "../LockInfo/LockInfo";
 
 export default function JuryForm() {
     const { t } = useTranslation();
     const classes = useStyles();
 
     const [currentTopic, setCurrentTopic] = useState<Topic>(undefined);
+    const [currentQuantity, setCurrentQuantity] = useState<string>(undefined);
+    const [selectedTopics, setSelectedTopics] = useState<{ topic: Topic, quantity: number }[]>([])
+    const [errorMenssage, setErorMenssage] = useState<string>(undefined)
+    const [cost, setCost] = useState<number>(0);
 
     const onChange = (t: Topic) => { setCurrentTopic(t) }
+    const onAdd = () => {
+        if (currentTopic && currentQuantity) {
+            setSelectedTopics(topics => {
+                setErorMenssage(undefined)
+                setCost(c => c + currentTopic.cost * (+currentQuantity))
+                return [...topics, { topic: currentTopic, quantity: +currentQuantity }]
+            })
+        }
+        else {
+            setErorMenssage(t("jury-form-incomplete"))
+        }
+    }
+    const onSummit = () => {
+        
+    }
 
     return (
         <div className={classes.rowContainer}>
             <Container title={t("registry")}>
-                <SelectTopic selectedOption={currentTopic} onChange={onChange}/>
+                <div className={classes.rowContainer}>
+                    <SelectTopic selectedOption={currentTopic} onChange={onChange} />
+                    <TextField variant="outlined" label={t("quantity")} value={currentQuantity}
+                        onChange={(event) => { setCurrentQuantity(event.target.value) }}
+                        InputProps={{ inputComponent: NumberFormatCustom as any }} />
+                    <Button onClick={onAdd} className={classes.button}>
+                        <Typography variant="h6">{t("add")}</Typography>
+                    </Button>
+                </div>
+                {errorMenssage && <Typography className={classes.error}>{errorMenssage}</Typography>}
+                {
+                    selectedTopics.map((t, i) => {
+                        const onRemove = () => {
+                            setSelectedTopics(topic => {
+                                const newTopics = [...topic]
+                                newTopics.splice(i, 1)
+                                setCost(c => c - t.topic.cost * t.quantity)
+                                return newTopics
+                            })
+                        }
+                        return (
+                            <div className={classnames(classes.rowContainer, classes.spaceBetween)}>
+                                <Typography variant="h6">{t.topic.name}</Typography>
+                                <Typography variant="h6">{t.quantity}</Typography>
+                                <IconButton className={classes.iconButton} onClick={onRemove}>
+                                    <CloseIcon />
+                                </IconButton>
+                            </div>
+                        )
+                    })
+                }
+                {selectedTopics.length !== 0 && <LockInfo lockCost={cost} />}
+                <div className={classes.rowContainer}>
+                    <Button className={classes.buttonRegistry} onClick={onSummit}
+                        disabled={selectedTopics.length === 0}>
+                        <Typography variant="h6">{t("regiter-jury")}</Typography>
+                    </Button>
+                </div>
             </Container>
         </div>
+    )
+}
+
+interface NumberFormatCustomProps {
+    inputRef: (instance: NumberFormat | null) => void;
+    onChange: (event: { target: { name: string; value: string } }) => void;
+    name: string;
+}
+
+function NumberFormatCustom(props: NumberFormatCustomProps) {
+    const { inputRef, onChange, ...other } = props;
+    return (
+        <NumberFormat
+            {...other}
+            getInputRef={inputRef}
+            onValueChange={(values: any) => {
+                onChange({
+                    target: {
+                        name: props.name,
+                        value: values.value,
+                    },
+                });
+            }}
+            thousandSeparator
+        />
     )
 }
--- src/entrypoint/presenters/web/components/JuryForm/styles.jsx
+++ src/entrypoint/presenters/web/components/JuryForm/styles.jsx
@@ -5,6 +5,53 @@ export default makeStyles(theme => ({
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
+        paddingTop: "1.1rem",
+        paddingBottom: "1.1rem",
+        "& .MuiOutlinedInput-root": {
+            borderRadius: 15,
+        },
+        "& .MuiOutlinedInput-notchedOutline": {
+            borderRadius: 15,
+        },
+    },
+    button: {
+        textTransform: "none",
+        backgroundColor: "none",
+        color: "#000000",
+        borderRadius: 15,
+        border: `1px solid ${theme.palette.primary.main}`,
+        "&:hover": {
+            backgroundColor: "#edeef2",
+            border: `1px solid ${theme.palette.primary.main}`,
+        }
+    },
+    error: {
+        color: "#ff0033"
+    },
+    iconButton: {
+        float: "right",
+        padding: "0",
+    },
+    spaceBetween: {
+        justifyContent: 'space-between',
+        borderRadius: 15,
+        border: `1px solid ${theme.palette.primary.main}`,
+        marginBottom: "0.5rem",
         padding: "1.1rem",
+    },
+    buttonRegistry: {
+        textTransform: "none",
+        color: "#FFFFFF",
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: 15,
+        border: `1px solid ${theme.palette.primary.main}`,
+        width: "100%",
+        "&:hover": {
+            backgroundColor: theme.palette.primary.light
+        },
+        "&.MuiButton-root.Mui-disabled": {
+            backgroundColor: "#FFFFFF",
+            border: "none"
+        }
     }
 }));
