e6d5c0ad7079f634ac32d1cebe32c5fb85907fd8
Agustin Dammiano <adammiano@itba.edu.ar>
2021-05-29 17:18:44 -0300

    minor fixes

--- src/entrypoint/presenters/web/components/NewsForm/NewsForm.tsx
+++ src/entrypoint/presenters/web/components/NewsForm/NewsForm.tsx
@@ -7,35 +7,28 @@ import SelectTopic from "../SelectTopic/SelectTopic";
 import Container from "../Container/Container";
 import LockInfo from "../LockInfo/LockInfo";
 import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
 
-export default function NewsForm() { //TODO
+export default function NewsForm() {
     const { t } = useTranslation();
     const classes = useStyles();
 
-    const [news, setNews] = useState<{ content: File, topic: Topic }>({ content: undefined, topic: undefined });
+    const [news, setNews] = useState<{ content: string, topic: Topic }>({ content: undefined, topic: undefined });
     const [preRender, setPreRender] = useState<boolean>(false);
-    const [file, setFile] = useState<string>(undefined);
+    const [fileName, setFileName] = useState<string>(undefined);
 
     const onChange = (t: Topic) => { setNews((n) => { return { ...n, topic: t } }) }
     const handleChangePreRender = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.checked) {
-            loadFile(news.content)
-        }
         setPreRender(event.target.checked);
     };
 
-    const loadFile = (file: File) => {
-        file.text().then(s => setFile(s))
-    }
-
     const setContent = (e: any) => {
         const target = (e.target as HTMLInputElement)
-        setNews((n) => {
-            if (preRender) {
-                loadFile(target.files[0])
-            }
-            return { ...n, content: target.files[0] }
-        })
+        setFileName(target.files[0].name)
+        target.files[0].text().then(s =>
+            setNews((n) => {
+                return { ...n, content: s }
+            }))
     }
 
     const onSummit = () => {
@@ -48,7 +41,7 @@ export default function NewsForm() { //TODO
                 <Container title={t("publish-news")}>
                     <div className={classes.rowContainer}>
                         <div className={classes.textContainer}>
-                            <Typography className={classes.text} variant="h6">{news.content ? news.content.name : t("select-markdown")}</Typography>
+                            <Typography className={classes.text} variant="h6">{fileName ? fileName : t("select-markdown")}</Typography>
                         </div>
                         <Fragment>
                             <input color="primary" accept="text/markdown" type="file" onChange={setContent} id="button-file" style={{ display: 'none', }} />
@@ -70,17 +63,16 @@ export default function NewsForm() { //TODO
                     <div className={classes.rowContainer}>
                         <Button className={classes.buttonPublish} onClick={onSummit}
                             disabled={!news.content || !news.topic}>
-                            <Typography variant="h6">{t("regiter-jury")}</Typography>
+                            <Typography variant="h6">{t("publish")}</Typography>
                         </Button>
                     </div>
                 </Container>
             </div>
             {
-                preRender && !!file &&
+                preRender && !!news.content &&
                 <div className={classes.rowContainer}>
                     <Container>
-
-                        <ReactMarkdown>{file}</ReactMarkdown>
+                        <ReactMarkdown remarkPlugins={[gfm]}>{news.content}</ReactMarkdown>
                     </Container>
                 </div>
             }
